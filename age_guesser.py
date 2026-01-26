563977db31b7bcc77a8d280aae69ba1b81c20858
Hyrum Lutz <d00524631@utahtech.edu>
2026-01-25 21:08:37 -0700

    Tried something... getting errors

--- age_guesser.py
+++ age_guesser.py
@@ -4,25 +4,31 @@ def guess_age():
     print("Hello! I'm a digital psychic, and I'm going to try to guess your age.")
     
     name = input("First, what is your name? ")
+    lowest = 15
+    highest = 100
+    possible_ages = list(range(lowest, highest))
+    shuff = random.shuffle(possible_ages)
+    age = shuff[0]
     
-    possible_ages = list(range(15, 31))
-    random.shuffle(possible_ages)
-    
-    for age in possible_ages:
-        answer = input(f"Are you {age} years old? (y/n): ").lower()
-        
-        if answer == 'y':
-            print(f"Hooray! I knew it! {name} is {age} years old.")
-            return
+    answer = 'n'
+    while answer != 'y':
+        change = input(f"Are you older or younger than {age}? (o/y): ").lower()
+        if change == 'o':
+            possible_ages = list(range(age, highest))
+            youngest = age
+            shuff = random.shuffle(possible_ages)
+            age = shuff[0]
+            answer = input(f"Are you {age} years old? (y/n): ").lower()
+        elif change == 'y':
+            possible_ages = list(range(lowest, age))
+            highest = age
+            shuff = random.shuffle(possible_ages)
+            age = shuff[0]
+            answer = input(f"Are you {age} years old? (y/n): ").lower()
 
-        if answer == 'n':
-            change = input(f"Are you older or younger than {age}? (o/y): ").lower()
-            if
-            
-        else:
-            print("Rats.")
-            
-    print("Well, I've run out of guesses! You're harder to read than I thought.")
+    if answer == 'y':
+        print(f"Hooray! I knew it! {name} is {age} years old.")
+        return
 
 if __name__ == "__main__":
     guess_age()
